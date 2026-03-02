1b2edd0e27c9920ba7b7215a35f6d62ff974fd04
Koky2701 <90915184+Koky2701@users.noreply.github.com>
2026-03-02 14:55:22 +0100

    Added report decode errors property (#195)
    Summary: 'report-decode-errors' and 'queued-frames' properties missing from RialtoMSEVideoSink
    Type: Feature
    Test Plan: UT/CT, Fullstack
    Jira: RDKEMW-12692

--- source/GStreamerMSEMediaPlayerClient.h
+++ source/GStreamerMSEMediaPlayerClient.h
@@ -269,7 +269,9 @@ public:
 
     int64_t getPosition(int32_t sourceId);
     bool setImmediateOutput(int32_t sourceId, bool immediateOutput);
+    bool setReportDecodeErrors(int32_t sourceId, bool reportDecodeErrors);
     bool getImmediateOutput(int32_t sourceId, bool &immediateOutput);
+    bool getQueuedFrames(int32_t sourceId, uint32_t &queuedFrames);
     bool getStats(int32_t sourceId, uint64_t &renderedFrames, uint64_t &droppedFrames);
 
     firebolt::rialto::AddSegmentStatus
