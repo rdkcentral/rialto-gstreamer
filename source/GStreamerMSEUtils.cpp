4a85a60d33b0f60a4651b18023dcd0b6886a291e
Ganeshpandian Usha <uganes412@comcast.com>
2026-07-30 11:47:21 +0000

    RDKEMW-15078: update on logs and coverity

--- source/GStreamerMSEUtils.cpp
+++ source/GStreamerMSEUtils.cpp
@@ -195,7 +195,7 @@ bool rialto_mse_sink_setup_supported_caps(GstElementClass *elementClass,
 {
     if (videoCapabilities.capabilities.empty())
     {
-        GST_DEBUG("No video capabilities provided (no HFP config)");
+        GST_WARNING("No video capabilities provided");
         return false;
     }
     GstCaps *caps = gst_caps_new_empty();
