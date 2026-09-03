566f092e972d66452c889693bcb394a6f1e59b52
uganes412_comcast <Usha_Ganeshpandian@comcast.com>
2026-09-03 13:26:01 +0530

    RDKEMW-23564: clang issue fix

--- tests/mocks/MediaCapabilitiesMock.h
+++ tests/mocks/MediaCapabilitiesMock.h
@@ -27,7 +27,7 @@ namespace firebolt::rialto
 class MediaCapabilitiesFactoryMock : public IMediaCapabilitiesFactory
 {
 public:
-    MOCK_METHOD(std::unique_ptr<IMediaCapabilities>, createMediaCapabilities, 
+    MOCK_METHOD(std::unique_ptr<IMediaCapabilities>, createMediaCapabilities,
                 (const std::optional<firebolt::rialto::common::AudioDecoderCapabilities> &preloadedAudio,
                  const std::optional<firebolt::rialto::common::VideoDecoderCapabilities> &preloadedVideo),
                 (const, override));
